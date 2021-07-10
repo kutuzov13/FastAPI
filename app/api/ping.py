78f6e3131b29bdcd1c6e7acf06f42c288ea5c220
kutuzov13 <kutuzov-1313@yandex.ru>
2021-07-10 21:15:31 +0300

    test

--- app/api/ping.py
+++ app/api/ping.py
@@ -8,7 +8,7 @@ router = APIRouter()
 async def pong(settings: Setting = Depends(get_setting)):
     """Get information about the working environment."""
     return {
-        'ping': 'pong!',
+        'ping': 'pong',
         'environment': settings.environment,
         'testing': settings.testing,
     }
--- tests/test_ping.py
+++ tests/test_ping.py
@@ -5,4 +5,4 @@ def test_ping(test_app):
     """Test main url /ping."""
     response = test_app.get('/ping')
     assert response.status_code == SUCCESSFUL_CODE
-    assert response.json() == {'environment': 'dev', 'ping': 'pong!', 'testing': True}
+    assert response.json() == {'environment': 'dev', 'ping': 'pong', 'testing': True}
